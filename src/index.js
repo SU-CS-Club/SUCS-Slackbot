703b040ecde8784e04afc17e01fa1df927769596
Daniel Whitney <whitneyd@southwestern.edu>
2020-09-06 13:18:14 -0500

    prolang command works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const SlackBot = require("slackbots");
 const axios = require("axios");
+const fs = require("fs");
 require("dotenv").config();
 
 const bot = new SlackBot({
@@ -21,15 +22,34 @@ bot.on("message", (data) => {
   if (data.type !== "message") {
     return;
   }
-  messageEvent(data.text);
+  messageEvent(data.text, data.channel, data.user);
 });
 
-const messageEvent = (text) => {
+const messageEvent = (text, channel, user) => {
   if (text.includes(" prolang")) {
     /// get programming languages assignments
+    fromText("pl.txt", user);
   } else if (text.includes(" comporg")) {
     // get comp org assignments
   } else if (text.includes(" cyber")) {
     // get cyber assignments
   }
 };
+
+function fromText(textFile, user) {
+  fs.readFile(textFile, (err, data) => {
+    if (err) {
+      console.log(error);
+    }
+    const assignments = data.toString().split("\n");
+    assignmentString = "";
+    assignments.forEach((assignment) => {
+      assignmentString += assignment;
+      assignmentString += "\n";
+    });
+    bot.postMessage(
+      user,
+      `The upcoming assignments for Programming language are: \n ${assignmentString}`
+    );
+  });
+}
